4e363fd275a02534c99d720ba0f6bb0fd3915c4b
Frenly Bro <frenlybro@proton.me>
2024-12-14 00:59:02 +0100

    tmu -> fcs

--- bot/lang.go
+++ bot/lang.go
@@ -5,36 +5,36 @@ const (
 
 	lNewRef = "🔵 <b><u>New Referred User!</u></b>\n\nYou have a new referred user."
 
-	lNewRefTmu = "🔵 <b><u>New Referral TMU!</u></b>\n\nYou have received %.9f new referral TMU."
+	lNewRefFCS = "🔵 <b><u>New Referral FCS!</u></b>\n\nYou have received %.9f new referral FCS."
 
 	lNewUser = "<b><u>New Frenly App user.</u></b> 🚀"
 
-	lNewMint = "<b><u>New TMU mint:</u> <code>%.9f TMU</code></b> 🚀"
+	lNewMint = "<b><u>New FCS mint:</u> <code>%.9f FCS</code></b> 🚀"
 
 	lStats = `🔵 <b><u>Frenly App Basic Stats</u></b>
 
 <b>Miners:</b> <code>%d</code>
 <b>Active Miners:</b> <code>%d</code>
-<b>TMU:</b> <code>%.9f</code>
-<b>Reward TMU:</b> <code>%.9f</code>`
+<b>FCS:</b> <code>%.9f</code>
+<b>Reward FCS:</b> <code>%.9f</code>`
 
 	lRanks = "🔵 <b><u>Frenly App Rankings</u></b>\n"
 
 	lRestartMining = `Mine More TON! 🚀
 
-<b><u>Each referred miner increases your mining power by 25% of their TMU!</u></b>
+<b><u>Each referred miner increases your mining power by 25% of their FCS!</u></b>
 
-Invite your friends to Frenly App and you will receive 25% of their TMU. You can find your referral link in the app.
+Invite your friends to Frenly App and you will receive 25% of their FCS. You can find your referral link in the app.
 
 Click the button bellow to restart the mining cycle!`
 
 	lStartMining = `<b><u>Start Mining TON</u></b> 🚀
 
-Current price of 1 TMU (Ton Mining Unit) is 0.1 TON. In 3 days it will be 0.25 TON and in few months 1 TON.
+Current price of 1 FCS (Frenly Coin Share) is 0.1 TON. In 3 days it will be 0.25 TON and in few months 1 TON.
 
-<b><u>Start mining TON by clicking the button bellow and receive 0.01 TMU for free!</u></b>
+<b><u>Start mining TON by clicking the button bellow and receive 0.01 FCS for free!</u></b>
 
-<u>This is the magic of compound interest.</u> If you receive 0.01 TMU and compound daily, after one year you will have 0.38 TMU. After two years, you will have 14.27 TMU, after three years, you will have 539.39 TMU and after five years 770029.12 TMU.`
+<u>This is the magic of compound interest.</u> If you receive 0.01 FCS and compound daily, after one year you will have 0.38 FCS. After two years, you will have 14.27 FCS, after three years, you will have 539.39 FCS and after five years 770029.12 FCS.`
 
 	lCycleFinished = `Your daily mining cycle has ended. Please run it again to continue mining!
 
