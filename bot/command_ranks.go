736bb3e24dbcaaa1b86fe2e8e15cfafd87eb29fe
Frenly Bro <frenlybro@proton.me>
2024-12-18 01:02:56 +0100

    position of ranks caller

--- bot/command_ranks.go
+++ bot/command_ranks.go
@@ -7,14 +7,53 @@ import (
 )
 
 func commandRanks(c telebot.Context) error {
+	user := getUser(c.Sender().ID)
 	var users []*User
 	msg := lRanks
+	showCaller := true
 
-	db.Order("tmu desc").Limit(10).Find(&users)
+	db.Order("tmu desc").Find(&users)
 
-	for _, u := range users {
-		tmu := float64(u.TMU) / float64(Mul9)
-		msg += fmt.Sprintf("\n<b>%s</b> - <code>%.9f TMU (%d)</code>", u.Name, tmu, u.CompoundCount)
+	for i, u := range users {
+		if i < 10 {
+			if user.TelegramId == u.TelegramId {
+				showCaller = false
+				tmu := float64(u.TMU) / float64(Mul9)
+				msg += fmt.Sprintf("\n<b><i>%d - %s</i></b> - <code>%.9f TMU (%d)</code>", i+1, u.Name, tmu, u.CompoundCount)
+			} else {
+				tmu := float64(u.TMU) / float64(Mul9)
+				msg += fmt.Sprintf("\n<b>%d - %s</b> - <code>%.9f TMU (%d)</code>", i+1, u.Name, tmu, u.CompoundCount)
+			}
+		}
+	}
+
+	if showCaller {
+		msg += "\n..."
+
+		for i, u := range users {
+			if user.TelegramId == u.TelegramId {
+				tmu := float64(0)
+
+				if i > 0 {
+					u = users[i-1]
+
+					tmu = float64(u.TMU) / float64(Mul9)
+					msg += fmt.Sprintf("\n<b>%d - %s</b> - <code>%.9f TMU (%d)</code>", i, u.Name, tmu, u.CompoundCount)
+				}
+
+				u = users[i]
+
+				tmu = float64(u.TMU) / float64(Mul9)
+				msg += fmt.Sprintf("\n<b><i>%d - %s</i></b> - <code>%.9f TMU (%d)</code>", i+1, u.Name, tmu, u.CompoundCount)
+
+				if i < len(users) {
+					u = users[i+1]
+
+					tmu = float64(u.TMU) / float64(Mul9)
+					msg += fmt.Sprintf("\n<b>%d - %s</b> - <code>%.9f TMU (%d)</code>", i+2, u.Name, tmu, u.CompoundCount)
+				}
+			}
+		}
 	}
 
 	_, err := b.Send(c.Chat(), msg, telebot.NoPreview)
--- bot/monitor.go
+++ bot/monitor.go
@@ -16,7 +16,6 @@ func (m *Monitor) loadMiners() {
 
 func (m *Monitor) sendNotifications() {
 	// counter := 1
-	log.Printf("Num Miners: %d", len(m.Miners))
 	for _, miner := range m.Miners {
 		if m.isSending(miner) {
 			notifyEnd(miner.TelegramId)
