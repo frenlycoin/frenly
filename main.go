dfb54a32547ee74d1ab294f2a2f3737117295224
Frenly Bro <frenlybro@proton.me>
2026-07-16 02:47:10 +0200

    amm work

--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"frenly/bot"
 )
 
 func main() {
 	bot.Start()
 
-	// amountOut, price, err := bot.CalculateTradePrice(200, 20, 60)
-	// if err != nil {
-	// 	fmt.Println("Error:", err)
-	// 	return
-	// }
+	amountOut, price, err := bot.CalculateTradePrice(200, 20, 60)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
 
-	// fmt.Printf("Input: 5 FREN\n")
-	// fmt.Printf("Output: %.4f TON\n", amountOut)
-	// fmt.Printf("Effective Price: %.4f TON per FREN\n", price)
+	fmt.Printf("Input: 5 FREN\n")
+	fmt.Printf("Output: %.4f TON\n", amountOut)
+	fmt.Printf("Effective Price: %.4f TON per FREN\n", price)
 }
